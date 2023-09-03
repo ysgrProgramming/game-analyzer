893406b43acdfea3f7664fe29e84a62cd86dc6e6
magnate <y.mizutani_1205@icloud.com>
2023-09-03 13:25:42 +0900

    make game tree constructor

---
+++ analyzer.py
@@ -0,0 +1,116 @@
+import numpy as np 
+from collections import deque
+
+class Analyzer():
+    def __init__(self, init_mat, max_mat, next_func, sym_func, end_func):
+        self.dim = len(init_mat.shape)
+        self.init_mat = init_mat
+        self.max_mat = max_mat
+        if self.init_mat.shape != self.max_mat.shape:
+            raise Exception("init_mat and max_mat shape mismatched")
+        
+        self.base1_mat, self.base2_mat = self._create_base_mat(max_mat)
+        self.max_hash = np.max(self.base2_mat)
+
+        self.next_func = next_func
+        self.sym_func = sym_func
+        self.end_func = end_func
+
+        self.graph = None
+        self.graph_inv = None
+        self.hash_list = None
+        self.hash_dict = None
+        self.dist_mat = None
+
+    def _create_base_mat(self, max_mat):
+        s = max_mat.shape
+        flat_mat = max_mat.ravel()
+        ac_list = [1]
+        for num in flat_mat:
+            ac_list.append(num*ac_list[-1])
+        ac_mat = np.array(ac_list)
+
+        base1_mat = ac_mat[:-1].reshape(s)
+        base2_mat = ac_mat[1:].reshape(s)
+
+        return base1_mat, base2_mat
+    
+    def mat_to_hash(self, mat):
+        hash = np.sum(mat * self.base1_mat)
+        return hash
+    
+    def hash_to_mat(self, hash):
+        mat = hash % self.base2_mat // self.base1_mat
+        return mat
+
+    def construct_game_graph(self):
+        init_hash = self.mat_to_hash(self.init_mat)
+        self.graph = [[]]
+        self.graph_inv = [[]]
+        self.hash_list = [init_hash]
+        self.hash_dict = {init_hash: 0}
+        todo = [0]
+        while todo:
+            ind = todo.pop()
+            hash = self.hash_list[ind]
+            mat = self.hash_to_mat(hash)
+
+            if self.end_func(mat) != None: continue
+            for next_mat in self.next_func(mat):
+                next_hash = self.mat_to_hash(next_mat)
+                for sym_mat in self.sym_func(next_mat):
+                    next_hash = min(next_hash, self.mat_to_hash(sym_mat))
+                    if next_hash in self.hash_dict:
+                        next_ind = self.hash_dict[next_hash]
+                        self.graph[ind].append(next_ind)
+                        self.graph_inv[next_ind].append(ind)
+                        break
+                else:
+                    next_ind = len(self.hash_list)
+                    self.hash_list.append(next_hash)
+                    self.hash_dict[next_hash] = next_ind
+                    self.graph.append([])
+                    self.graph_inv.append([])
+                    self.graph[ind].append(next_ind)
+                    self.graph_inv[next_ind].append(ind)
+                    todo.append(next_ind)
+        self.state_num = len(self.hash_list)
+    
+    def make_tree(self):
+        self.dist_mat = np.full(self.state_num, -1, np.int32)
+        self.judge_mat = np.full(self.state_num, -1, np.int8)
+        child_count_mat = np.empty(self.state_num, np.int32)
+
+        todo = deque([])
+        for i in range(self.state_num):
+            child_count_mat[i] = len(self.graph[i])
+            if child_count_mat[i] == 0:
+                self.dist_mat[i] = 0
+                todo.append(i)
+
+                hash = self.hash_list[i]
+                mat = self.hash_to_mat(hash)
+                self.judge_mat[i] = self.end_func(mat)+1
+        
+        while todo:
+            ind = todo.popleft()
+            dist = self.dist_mat[ind]
+            judge = self.judge_mat[ind]
+            if judge != 0:
+                for node in self.graph_inv[ind]:
+                    if self.dist_mat[node] != -1: continue
+                    child_count_mat[node] -= 1
+                    self.judge_mat[node] = max(self.judge_mat[node], -judge+2)
+                    if child_count_mat[node] == 0:
+                        self.dist_mat[node] = dist + 1
+                        todo.append(node)
+            else:
+                for node in self.graph_inv[ind]:
+                    if self.dist_mat[node] != -1: continue
+                    self.judge_mat[node] = 2
+                    self.dist_mat[node] = dist + 1
+                    todo.append(node)
+        
+        self.judge_mat[self.judge_mat == -1] = 1
+        self.judge_mat -= 1
+    
---
+++ main.py
@@ -0,0 +1,45 @@
+import numpy as np
+from analyzer import Analyzer
+
+init_mat = np.array([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+])
+max_mat = np.array([
+    [3, 3, 3],
+    [3, 3, 3],
+    [3, 3, 3]
+])
+
+def sym_func(mat):
+    flip_mat = np.flip(mat)
+    for i in range(4):
+        yield(np.rot90(mat, i))
+        yield(np.rot90(flip_mat, i))
+
+def end_func(mat):
+    for i in range(4):
+        rot_mat = np.rot90(mat, i)
+        if rot_mat[0][0] == rot_mat[1][1] == rot_mat[2][2] == 2:
+            return -1
+        elif rot_mat[0][0] == rot_mat[0][1] == rot_mat[0][2] == 2:
+            return -1
+        elif rot_mat[1][0] == rot_mat[1][1] == rot_mat[1][2] == 2:
+            return -1
+    if np.count_nonzero(rot_mat == 0) == 0: return 0
+    return None
+
+def next_func(mat):
+    mat = mat*2%3
+    for y in range(3):
+        for x in range(3):
+            if mat[y][x] == 0:
+                new_mat = mat.copy()
+                new_mat[y][x] = 1
+                yield new_mat
+
+
+an = Analyzer(init_mat, max_mat, next_func, sym_func, end_func)
+an.construct_game_graph()
+an.make_tree()
