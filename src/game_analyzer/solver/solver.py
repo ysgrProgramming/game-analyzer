2451c338e8733d5ae40c8eedfab99395b26e3aee
magnate <y.mizutani_1205@icloud.com>
2024-12-11 09:09:43 +0900

    Refactoring codes

--- src/game_analyzer/solver/solver.py
+++ src/game_analyzer/solver/solver.py
@@ -9,15 +9,7 @@ from array import array
 import time
 import sys
 
-sys.setrecursionlimit(10**5)
-
-
-def sign(x: int) -> int:
-    if x > 0:
-        return 1
-    if x < 0:
-        return -1
-    return 0
+sys.setrecursionlimit(10**9)
 
 
 @dataclass
@@ -53,68 +45,83 @@ class Solver:
             ra_time=ra_time,
         )
 
-    def _search_game_graph(self, state: State, idx: int) -> None:
-        for next_state in self._game.find_next_states(state):
-            next_hash = next_state.to_hash()
-            if next_hash in self._hash_dict:
-                next_idx = self._hash_dict[next_hash]
-            else:
-                next_idx = self._register_state(next_state)
-                next_res = self._game.evaluate_state(next_state)
-                if next_res is not None:
-                    self._eval_list[next_idx] = next_res
-                    self._depth_list[idx] = 0
-                else:
-                    self._search_game_graph(next_state, next_idx)
-            self._child_count_list[idx] += 1
-            self._graph_inv[next_idx].append(idx)
-        if self._child_count_list[idx] == 0:
-            self._eval_list[idx] = self._game.default_eval
-            self._depth_list[idx] = 0
-
-    def _retrograde_analyze(self) -> None:  # noqa: C901
-        for idx in range(self.node_size):
-            if self._child_count_list[idx] == 0:
-                self._confirm_eval(idx)
-        while self._key_list:
-            key = heappop(self._key_list)
-            for idx in self._queue_dict[key]:
-                self._confirm_eval(idx)
-        for idx in range(self.node_size):
-            if self._child_count_list[idx] > 0:
-                self._eval_list[idx] = 0
-                self._depth_list[idx] = -1
-
     def _register_state(self, state: State) -> int:
         idx = self.node_size
+        hash_dict = self._hash_dict
         for mirror_state in self._game.find_mirror_states(state):
             state_hash: int = mirror_state.to_hash()  # type: ignore
-            if state_hash in self._hash_dict and self._hash_dict[state_hash] != idx:
+            if state_hash in hash_dict and hash_dict[state_hash] != idx:
                 msg = "mirror func error"
                 raise ValueError(msg)
-            self._hash_dict[state_hash] = idx
+            hash_dict[state_hash] = idx
         self._eval_list.append(0)
         self._depth_list.append(-1)
         self._graph_inv.append([])
         self._child_count_list.append(0)
         return idx
 
+    def _search_game_graph(self, state: State, idx: int) -> None:
+        eval_list = self._eval_list
+        depth_list = self._depth_list
+        child_count_list = self._child_count_list
+        graph_inv = self._graph_inv
+        hash_dict = self._hash_dict
+        evaluate_state = self._game.evaluate_state
+
+        for next_state in self._game.find_next_states(state):
+            next_hash = next_state.to_hash()
+            if next_hash in hash_dict:
+                next_idx = hash_dict[next_hash]
+            else:
+                next_idx = self._register_state(next_state)
+                next_res = evaluate_state(next_state)
+                if next_res is not None:
+                    eval_list[next_idx] = next_res
+                    depth_list[idx] = 0
+                else:
+                    self._search_game_graph(next_state, next_idx)
+            child_count_list[idx] += 1
+            graph_inv[next_idx].append(idx)
+        if child_count_list[idx] == 0:
+            eval_list[idx] = self._game.default_eval
+            depth_list[idx] = 0
+
+    def _retrograde_analyze(self) -> None:  # noqa: C901
+        child_count_list = self._child_count_list
+        confirm_eval = self._confirm_eval
+        for idx in range(self.node_size):
+            if child_count_list[idx] == 0:
+                confirm_eval(idx)
+        while self._key_list:
+            key = heappop(self._key_list)
+            queue = self._queue_dict.pop(key)
+            for idx in queue:
+                confirm_eval(idx)
+        for idx in range(self.node_size):
+            if child_count_list[idx] > 0:
+                self._eval_list[idx] = 0
+                self._depth_list[idx] = -1
+
     def _confirm_eval(self, idx: int):  # noqa: C901
-        prev_ev, prev_depth = -self._eval_list[idx], self._depth_list[idx] + 1
+        eval_list = self._eval_list
+        depth_list = self._depth_list
+        child_count_list = self._child_count_list
+        prev_ev, prev_depth = -eval_list[idx], depth_list[idx] + 1
         for prev_idx in self._graph_inv[idx]:
-            if self._child_count_list[prev_idx] == 0:
+            if child_count_list[prev_idx] == 0:
                 continue
-            self._child_count_list[prev_idx] -= 1
-            if self._is_eval_bigger(prev_ev, prev_depth, prev_idx):
-                self._eval_list[prev_idx] = prev_ev
-                self._depth_list[prev_idx] = prev_depth
+            child_count_list[prev_idx] -= 1
+            if self._is_better_eval(prev_ev, prev_depth, prev_idx):
+                eval_list[prev_idx] = prev_ev
+                depth_list[prev_idx] = prev_depth
                 if prev_ev >= 0:
                     self._add_to_queue(prev_idx)
-            if self._child_count_list[prev_idx] == 0:
+            if child_count_list[prev_idx] == 0:
                 self._confirm_eval(prev_idx)
+        child_count_list[idx] = 0
         self._graph_inv[idx].clear()
 
-    def _is_eval_bigger(self, ev: int, depth: int, idx: int):
+    def _is_better_eval(self, ev: int, depth: int, idx: int):
         if self._depth_list[idx] == -1:
             return True
         if self._eval_list[idx] < ev:
@@ -124,8 +131,9 @@ class Solver:
     def _add_to_queue(self, idx: int):
         ev, depth = self._eval_list[idx], self._depth_list[idx]
         key = (-ev, ev * depth)
-        if key in self._queue_dict:
-            self._queue_dict[key].append(idx)
-        else:
-            self._queue_dict[key] = [idx]
+        lst = self._queue_dict.get(key)
+        if key not in self._queue_dict:
+            lst = []
+            self._queue_dict[key] = lst
             heappush(self._key_list, key)
+        self._queue_dict[key].append(idx)
